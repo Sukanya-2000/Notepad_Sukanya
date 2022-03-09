8b3e31e6027a21f9279ac7e8bae401d2b306ef94
Sukanya-2000 <67016572+Sukanya-2000@users.noreply.github.com>
2022-03-09 04:57:32 -0500

    Create aboutme.h

---
+++ includes/aboutme.h
@@ -0,0 +1,26 @@
+#ifndef ABOUTME_H
+#define ABOUTME_H
+
+#include <QDialog>
+#include <QMainWindow>
+#include <QDesktopServices>
+
+namespace Ui {
+class AboutMe;
+}
+
+class AboutMe : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit AboutMe(QWidget *parent = nullptr);
+    ~AboutMe();
+
+
+
+private:
+    Ui::AboutMe *ui;
+};
+
+#endif // ABOUTME_H
